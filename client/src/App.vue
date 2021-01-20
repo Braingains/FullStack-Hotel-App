350f533a3700fbdee535b73318a4878093e1dfe2
Ewan Brain <ewan.brain@gmail.com>
2021-01-20 10:50:54 +0000

    api fetch working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>
+    <p>APP.VUE</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BookingService from './services/BookingService.js'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      bookings: []
+    };
+  },
+  mounted() {
+    this.fetchBookings();
+  },
+
+  methods: {
+    fetchBookings(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
