a49f689842a65d0a5111acfc6b14416f7942cbaa
Ewan Brain <ewan.brain@gmail.com>
2021-01-20 11:09:19 +0000

    add booking form showing on page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <p>APP.VUE</p>
+    <bookings-form/>
     </div>
 </template>
 
 <script>
 import BookingService from './services/BookingService.js'
+import BookingsForm from './components/BookingsForm.vue'
+
 export default {
   name: "app",
+  components: {
+    'bookings-form': BookingsForm
+  },
+
   data() {
     return {
       bookings: []
---
+++ client/src/components/BookingsForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form id="bookings-form" method="post">
+      <h2> Add Booking </h2>
+    <div class="inputForm">
+      <label for="name">Name:</label>
+      <input type="text" id="name" required/>
+    </div>
+    <div class="inputForm">
+      <label for="email">Email:</label>
+      <input type="email" id="email" required/>
+    </div>
+    <div class="inputForm">
+      <label for="checkinTrue">Checked in</label>
+      <input type="radio" id="checkinTrue" name="checkin" value="checked in">
+      <label for="checkinFalse">Not Checked in</label>
+      <input type="radio" id="checkinFalse" name="checkin" value="not checkeded in">
+    </div>
+  </form>
+  
+
+
+</template>
+
+<script>
+export default {
+    name: 'bookings-form',
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
